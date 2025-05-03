0104fd38d634a832235dc91a4802146efd2d04b9
shin0112 <wnslcosltimo12@gmail.com>
2025-05-03 19:18:05 +0900

    refactor: vector store 함수 이동

--- src/RAG.py
+++ src/RAG.py
@@ -1,16 +1,6 @@
-import os
-import re
 import logging
-from pathlib import Path
 import torch
-from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM
-from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_community.vectorstores.utils import DistanceStrategy
-from langchain_huggingface import HuggingFaceEmbeddings
 
-import get_ko_law
 import get_data
 import model_loader
 import query_processor
@@ -20,14 +10,6 @@ from prompts import prompt_v3_cot_fewshot
 # Configure logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Paths
-FAISS_PATH = Path(__file__).parent.parent / 'data' / 'faiss_index'
-GUIDELINE_FAISS_PATH = FAISS_PATH / "guideline"
-LAW_FAISS_PATH = FAISS_PATH / "law"
-LAW_FILE_PATH = Path(__file__).parent.parent / 'data' / 'law_file_paths.json'
-# PROMPT_PATH = Path(__file__).parent / 'prompts' / 'prompt_v3_cot_fewshot.txt'
-
 
 def load_prompt():
     # todo: langchain prompt 사용해보기
@@ -40,72 +22,6 @@ def logging_model(model_name, embeddings_model, retriever_strategy, num_articles
     logging.info(f"  검색 전략: {retriever_strategy}")
     logging.info(f"  검색 문서 수: {num_articles}")
     logging.info(f"  프롬프트 버전: {prompt_version}")
-
-def load_or_create_faiss_guideline(embeddings_model):
-    if GUIDELINE_FAISS_PATH.exists():
-        logging.info("FAISS 인덱스를 로드 중입니다...")
-        guideline_store = FAISS.load_local(
-            GUIDELINE_FAISS_PATH,
-            embeddings_model,
-            allow_dangerous_deserialization=True
-        )
-        logging.info("FAISS 인덱스 로드가 완료되었습니다!")
-    else:
-        logging.info("한국 법률 데이터를 로드 중입니다...")
-        ko_guideline_docs_list = get_ko_law.get_ko_guideline()
-        ko_law_docs_list = get_ko_law.get_ko_law()
-
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len,
-        )
-
-        logging.info("문서를 분할 중입니다...")
-        ko_split_guideline_docs_list = [
-            chunk for docs in ko_guideline_docs_list for chunk in text_splitter.split_documents(docs)
-        ]
-        ko_split_law_docs_list = [
-            chunk for docs in ko_law_docs_list for chunk in text_splitter.split_documents(docs)
-        ]
-        logging.info("문서 분할이 완료되었습니다!")
-
-        logging.info("guideline 문서 저장 중...")
-        guideline_store = FAISS.from_documents(
-            documents=ko_split_guideline_docs_list,
-            embedding=embeddings_model,
-            distance_strategy=DistanceStrategy.COSINE
-        )
-        logging.info("guideline 인덱스 생성 완료")
-
-        logging.info("법률 문서 저장 중...")
-        law_store = FAISS.from_documents(
-            documents=ko_split_law_docs_list,
-            embedding=embeddings_model,
-            distance_strategy=DistanceStrategy.COSINE
-        )
-        logging.info("법률 인덱스 생성 완료")
-
-        FAISS_PATH.mkdir(parents=True, exist_ok=True)
-        GUIDELINE_FAISS_PATH.mkdir(parents=True, exist_ok=True)
-        LAW_FAISS_PATH.mkdir(parents=True, exist_ok=True)
-        guideline_store.save_local(GUIDELINE_FAISS_PATH)
-        law_store.save_local(LAW_FAISS_PATH)
-
-        logging.info("FAISS 인덱스가 저장되었습니다!")
-
-    return guideline_store
-
-
-def load_or_create_faiss_law(embeddings_model):
-    logging.info("FAISS 인덱스를 로드 중입니다...")
-    law_store = FAISS.load_local(
-        LAW_FAISS_PATH,
-        embeddings_model,
-        allow_dangerous_deserialization=True
-    )
-    logging.info("FAISS 인덱스 로드가 완료되었습니다!")
-    return law_store
 
 
 def generate_answer(model, tokenizer, query, context):
@@ -147,10 +63,10 @@ def run_experiment(model_name, search_strategy: str = "double", num_articles: in
 
     # 0.2. 쿼리 및 문서 임베딩 모델
     embeddings_model = vectorStore.KoSimCSE()
-    guideline_store = load_or_create_faiss_guideline(embeddings_model)
+    guideline_store = vectorStore.load_or_create_faiss_guideline(embeddings_model)
 
     if search_strategy == "double":
-        law_store = load_or_create_faiss_law(embeddings_model)
+        law_store = vectorStore.load_or_create_faiss_law(embeddings_model)
         retriever_2nd = law_store.as_retriever(
             search_type="similarity",
             search_kwargs={"k": 2}
--- src/vectorStore.py
+++ src/vectorStore.py
@@ -1,6 +1,22 @@
 import logging
 import torch
+from pathlib import Path
+
 from transformers import AutoModel, AutoTokenizer
+from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.utils import DistanceStrategy
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+import get_ko_law
+
+# Paths
+FAISS_PATH = Path(__file__).parent.parent / 'data' / 'faiss_index'
+GUIDELINE_FAISS_PATH = FAISS_PATH / "guideline"
+LAW_FAISS_PATH = FAISS_PATH / "law"
+LAW_FILE_PATH = Path(__file__).parent.parent / 'data' / 'law_file_paths.json'
+# PROMPT_PATH = Path(__file__).parent / 'prompts' / 'prompt_v3_cot_fewshot.txt'
+
+
 
 class KoSimCSE:
     def __init__(self, model_name='BM-K/KoSimCSE-roberta', device=None):
@@ -26,3 +42,70 @@ class KoSimCSE:
 
     def embed_query(self, text: str) -> list[float]:
         return self.embed_documents([text])[0]
+
+
+def load_or_create_faiss_guideline(embeddings_model):
+    if GUIDELINE_FAISS_PATH.exists():
+        logging.info("FAISS 인덱스를 로드 중입니다...")
+        guideline_store = FAISS.load_local(
+            GUIDELINE_FAISS_PATH,
+            embeddings_model,
+            allow_dangerous_deserialization=True
+        )
+        logging.info("FAISS 인덱스 로드가 완료되었습니다!")
+    else:
+        logging.info("한국 법률 데이터를 로드 중입니다...")
+        ko_guideline_docs_list = get_ko_law.get_ko_guideline()
+        ko_law_docs_list = get_ko_law.get_ko_law()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len,
+        )
+
+        logging.info("문서를 분할 중입니다...")
+        ko_split_guideline_docs_list = [
+            chunk for docs in ko_guideline_docs_list for chunk in text_splitter.split_documents(docs)
+        ]
+        ko_split_law_docs_list = [
+            chunk for docs in ko_law_docs_list for chunk in text_splitter.split_documents(docs)
+        ]
+        logging.info("문서 분할이 완료되었습니다!")
+
+        logging.info("guideline 문서 저장 중...")
+        guideline_store = FAISS.from_documents(
+            documents=ko_split_guideline_docs_list,
+            embedding=embeddings_model,
+            distance_strategy=DistanceStrategy.COSINE
+        )
+        logging.info("guideline 인덱스 생성 완료")
+
+        logging.info("법률 문서 저장 중...")
+        law_store = FAISS.from_documents(
+            documents=ko_split_law_docs_list,
+            embedding=embeddings_model,
+            distance_strategy=DistanceStrategy.COSINE
+        )
+        logging.info("법률 인덱스 생성 완료")
+
+        FAISS_PATH.mkdir(parents=True, exist_ok=True)
+        GUIDELINE_FAISS_PATH.mkdir(parents=True, exist_ok=True)
+        LAW_FAISS_PATH.mkdir(parents=True, exist_ok=True)
+        guideline_store.save_local(GUIDELINE_FAISS_PATH)
+        law_store.save_local(LAW_FAISS_PATH)
+
+        logging.info("FAISS 인덱스가 저장되었습니다!")
+
+    return guideline_store
+
+
+def load_or_create_faiss_law(embeddings_model):
+    logging.info("FAISS 인덱스를 로드 중입니다...")
+    law_store = FAISS.load_local(
+        LAW_FAISS_PATH,
+        embeddings_model,
+        allow_dangerous_deserialization=True
+    )
+    logging.info("FAISS 인덱스 로드가 완료되었습니다!")
+    return law_store
