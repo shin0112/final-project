eb4b633abc1d20b6878f6be9054838b51eb2398e
shin0112 <wnslcosltimo12@gmail.com>
2025-05-04 19:06:33 +0900

    feat: rerank 로직 추가 및 class 생성

--- src/vectorStore.py
+++ src/vectorStore.py
@@ -1,3 +1,6 @@
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
+from langchain.retrievers.document_compressors import CrossEncoderReranker
 import logging
 import torch
 from pathlib import Path
@@ -15,6 +18,15 @@ GUIDELINE_FAISS_PATH = FAISS_PATH / "guideline"
 LAW_FAISS_PATH = FAISS_PATH / "law"
 LAW_FILE_PATH = Path(__file__).parent.parent / 'data' / 'law_file_paths.json'
 # PROMPT_PATH = Path(__file__).parent / 'prompts' / 'prompt_v3_cot_fewshot.txt'
+
+
+def get_faiss_paths(embedding_model_name: str):
+    base_path = FAISS_PATH / embedding_model_name.replace("/", "_")
+    return {
+        "guideline": base_path / "guideline",
+        "law": base_path / "law"
+    }
+
 
 class KoSimCSE:
     def __init__(self, model_name='BM-K/KoSimCSE-roberta', device=None):
@@ -42,11 +54,42 @@ class KoSimCSE:
         return self.embed_documents([text])[0]
 
 
-def load_or_create_faiss_guideline(embeddings_model):
-    if GUIDELINE_FAISS_PATH.exists():
-        logging.info("FAISS 인덱스를 로드 중입니다...")
+class MsMarcoDistilbert:
+    def __init__(self, model_name='sentence-transformers/msmarco-distilbert-dot-v5', device=None):
+        logging.info("MsMarcoDistilbert 임베딩 모델 로드 중입니다...")
+        self.device = "cuda"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name).to(self.device)
+        self.model.eval()
+        logging.info("MsMarcoDistilbert 모델 로드 완료!")
+
+    def __call__(self, text: str) -> list[float]:
+        return self.embed_query(text)
+
+    def embed_documents(self, texts) -> list[list[float]]:
+        inputs = self.tokenizer(
+            texts, padding=True, truncation=True, return_tensors="pt").to(self.device)
+        with torch.no_grad():
+            outputs = self.model(**inputs, return_dict=True)
+            embeddings = outputs.last_hidden_state[:, 0]
+            embeddings = embeddings / \
+                embeddings.norm(dim=1, keepdim=True)
+        return embeddings.cpu().tolist()
+
+    def embed_query(self, text: str) -> list[float]:
+        return self.embed_documents([text])[0]
+
+
+def load_or_create_faiss_guideline(embeddings_model, embedding_model_name):
+    paths = get_faiss_paths(embedding_model_name)
+    guideline_path = paths["guideline"]
+    law_path = paths["law"]
+
+    if guideline_path.exists():
+        logging.info(
+            f"임베딩 모델 {embedding_model_name}을 사용하여 FAISS 인덱스 로드를 시작합니다...")
         guideline_store = FAISS.load_local(
-            GUIDELINE_FAISS_PATH,
+            guideline_path,
             embeddings_model,
             allow_dangerous_deserialization=True
         )
@@ -90,20 +133,47 @@ def load_or_create_faiss_guideline(embeddings_model):
         FAISS_PATH.mkdir(parents=True, exist_ok=True)
         GUIDELINE_FAISS_PATH.mkdir(parents=True, exist_ok=True)
         LAW_FAISS_PATH.mkdir(parents=True, exist_ok=True)
-        guideline_store.save_local(GUIDELINE_FAISS_PATH)
-        law_store.save_local(LAW_FAISS_PATH)
+        guideline_path.parent.mkdir(parents=True, exist_ok=True)
+        guideline_store.save_local(guideline_path)
+        law_store.save_local(law_path)
 
         logging.info("FAISS 인덱스가 저장되었습니다!")
 
     return guideline_store
 
 
-def load_or_create_faiss_law(embeddings_model):
+def load_or_create_faiss_law(embeddings_model, embedding_model_name):
+    paths = get_faiss_paths(embedding_model_name)
+    law_path = paths["law"]
+
     logging.info("FAISS 인덱스를 로드 중입니다...")
     law_store = FAISS.load_local(
-        LAW_FAISS_PATH,
+        law_path,
         embeddings_model,
         allow_dangerous_deserialization=True
     )
     logging.info("FAISS 인덱스 로드가 완료되었습니다!")
     return law_store
+
+
+class BgeReranker:
+    model = HuggingFaceCrossEncoder(
+        model_name='BAAI/bge-reranker-v2-m3', device='cuda')
+    compressor = CrossEncoderReranker(
+        model=model,
+        search_kwargs={"k": 3},
+    )
+    compression_retriever = ContextualCompressionRetriever(
+        base_compressor=compressor,
+        base_retriever=load_or_create_faiss_guideline(MsMarcoDistilbert.as_retriever(
+            search_kwargs={"k": 10}
+        )),
+        return_source_documents=True,
+        search_kwargs={"k": 3},
+    )
+
+    def __init__(self):
+        logging.info("BgeReranker 모델 로드 중입니다...")
+        self.compressor = BgeReranker.compressor
+        self.compression_retriever = BgeReranker.compression_retriever
+        logging.info("BgeReranker 모델 로드 완료!")
