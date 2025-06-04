f196fd8c06c2b997018efa4250b5a045e46cfe0b
shin0112 <wnslcosltimo12@gmail.com>
2025-06-05 00:38:59 +0900

    feat: 로그인 함수 주속 처리 해제

--- src/load_token.py
+++ src/load_token.py
@@ -6,4 +6,4 @@ from huggingface_hub import login
 load_dotenv(dotenv_path=Path(__file__).parent.parent / ".env", override=True)
 os.environ["HF_HOME"] = "/data/wnslcosltimo12/hf_cache"
 token = os.environ.get("HUGGINGFACE_TOKEN")
-# login(token=token)
+login(token=token)
